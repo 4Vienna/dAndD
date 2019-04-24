1fd87ef3eac54de0bc0e87bb0f94473b67e2c146
4Vienna <viennalarose@gmail.com>
2019-04-24 10:01:20 -0600

    fix broken page

---
+++ src/components/pages/templates/campaign.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Campaign extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Tomb of Annihilation</h1>
+      </div>
+    );
+  }
+}
